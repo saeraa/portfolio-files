4b7a1eeb9696eaa27a5056a8bb835c5f4dfb1685
Sara <saeraa@gmail.com>
2022-06-23 17:37:22 +0200

    json update

--- src/Components/portfolioInfo.js
+++ src/Components/portfolioInfo.js
@@ -14,6 +14,33 @@ const portfolioInfo = [
         "description": "",
     }, */
     {
+        "name": "Memory Game",
+        "_id": "05",
+        "img": "",
+        "tools": [
+            "",
+            "",
+            ""
+        ],
+        "summary": "",
+        "link": "",
+        "repo-link": "https://github.com/saeraa/memory-game",
+        "description": "",
+    }, {
+        "name": "TV Shows Watchlist",
+        "_id": "04",
+        "img": ".././img/portfolio_watchlist.png",
+        "tools": [
+            "html",
+            "css",
+            "js",
+            "react"
+        ],
+        "summary": "A TV Show app for searching for shows and adding them to your watchlist.",
+        "link": "https://saera-watchlist.netlify.app/",
+        "repo-link": "https://github.com/saeraa/tvshows-react",
+        "description": "",
+    }, {
         "name": "Quizzical: JavaScript edition",
         "_id": "03",
         "img": ".././img/portfolio_quiz.png",
@@ -25,7 +52,7 @@ const portfolioInfo = [
         ],
         "summary": "A quiz app, with multiple choice questions about JavaScript.",
         "link": "https://saera-quizzical.netlify.app/",
-        "repo-link": "",
+        "repo-link": "https://github.com/saeraa/Quizzical",
         "description": "",
     }, {
         "name": "Hangman",
@@ -38,7 +65,7 @@ const portfolioInfo = [
         ],
         "summary": "Play the classic Hangman game.",
         "link": "https://saera-hangman.netlify.app/",
-        "repo-link": "",
+        "repo-link": "https://github.com/saeraa/hangman",
         "description": "A random word, wordlength 9-15 characters, from an API is chosen. \n For fun, the word definition is linked when the word is revealed or the game is won.",
     }, {
         "name": "Color Scheme Picker",
@@ -51,7 +78,7 @@ const portfolioInfo = [
         ],
         "summary": "A color scheme picker where you can choose the color and recieve a suggested color scheme from an API.",
         "link": "https://saera-colorschemepicker.netlify.app/",
-        "repo-link": "",
+        "repo-link": "https://github.com/saeraa/colorSchemePicker",
         "description": "",
     }
 
